0ac06220a900cc4b5e2cb0a57774606f1093f2fd
Eranmonnie <ajalaferanmi123@gmail.com>
2023-05-21 23:02:03 +0100

    binary search

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+class linear_search:
+    def __init__(self) :
+        self.arr = [1,2,3,4,5]
+
+    def search(self,val):
+        x = 0
+        while  x < len(self.arr):
+            if self.arr[x] == val:
+                return x
+            else:
+                x += 1
+        return -1
+lin = linear_search()
+print(lin.search(6))
